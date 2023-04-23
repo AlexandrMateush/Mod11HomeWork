1436fa59dd765f4428de4a09f869c02574ea9dc5
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-04-23 14:25:19 +0300

    Add files via upload

--- Mod11HomeWork/src/main/java/org/example/SortName.java
+++ Mod11HomeWork/src/main/java/org/example/SortName.java
@@ -1,26 +1,17 @@
 package org.example;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SortName {
     static List<String> names = Arrays.asList("Sasha ", "Dima ", "Vova ","Dasha","Misha");
-
-
     public static StringBuilder sortNames(List<String> names) {
-        StringBuilder result = new StringBuilder();
-        Collections.sort(names, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return s2.compareTo(s1);
-            }
-        });
+        return new StringBuilder(String.valueOf(names.stream()
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining("\n"))));
 
-        for (String string : names) {
-            result.append(string.toUpperCase()).append("\n");
-        }
-        return result;
     }
-
 
     public static void main(String[] args) {
         System.out.println((sortNames(names)));
